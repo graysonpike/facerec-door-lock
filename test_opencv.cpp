48f0817dc5ae878a2c4cffb460a9091e086aee0f
Grayson112233 <graysonpike@gmail.com>
2018-02-14 17:28:25 +0000

    Added basic verison checking

---
+++ test_opencv.cpp
@@ -0,0 +1,12 @@
+// This program should output the version of OpenCV.
+
+#include <iostream>
+#include <cv.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    cout << "Version: " << CV_VERSION << endl;
+
+}
